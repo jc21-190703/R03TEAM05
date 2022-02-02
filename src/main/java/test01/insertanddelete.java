100d32e8ead428442399f803fd6dbe612edd0d23
龍之介 前川 <190443@jc-21.jp>
2022-02-02 11:56:02 +0900

    最新

--- src/main/java/test01/insertanddelete.java
+++ src/main/java/test01/insertanddelete.java
@@ -4,11 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
